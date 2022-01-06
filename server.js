7fcdc6c2a3acba64a2f6687e16cecb057b89a9b2
celedev97 <celedev97@gmail.com>
2022-01-06 18:36:50 +0100

    first version

--- server.js
+++ server.js
@@ -1,20 +1,41 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-
-var app = express();
-
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-});
+const express = require('express');
+const app = express();
+const multer = require('multer')
 
 
+//#region server setup
+//enable CORS so that my API is testable by FCC
+const cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));
+
+// static files (used only for CSS)
+app.use(express.static('public'));
+
+//#endregion
+
+//#region routes
+// route for the homepage
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+})
+
+const upfile = multer({}).single('upfile')
+app.post("/api/fileanalyse", (req, res) => {
+    upfile (req, res, function(err) {
+        res.json({
+            "name": req.file.originalname,
+            "type": req.file.mimetype,
+            "size": req.file.size,
+        })
+    })
+})
+
+//#endregion
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+
+
+// listen for requests
+const listener = app.listen(process.env.PORT || 8080, function () {
+    console.log('Your app is listening on port ' + listener.address().port);
 });
